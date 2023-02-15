8aac60a0fbfef54a90ec08523a7655b10f9baa12
zulkiflisaid <zulcompound@gmail.com>
2023-02-15 09:41:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <base href="http://localhost/vanila-router-master/zdatatable/" target="_blank"> 
+  <base href="http://localhost/zdatatable/" target="_blank"> 
   <link rel="stylesheet" href="<?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>public/css/style.css" >    
    
 <title>Go-POS</title>
